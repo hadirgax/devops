6a5f04700a4e9509d7a449b331ebadf721ca72fd
Hadir Garcia <halexandergc@gmail.com>
2024-05-25 16:03:50 -0300

    Update setup-env.sh script for Linux Ubuntu

--- setup_devenv/linux_ubuntu/setup-env.sh
+++ setup_devenv/linux_ubuntu/setup-env.sh
@@ -35,41 +35,7 @@ sudo rm -rf /tmp/git-${GIT_VERSION}
 sudo rm -rf /var/lib/apt/lists/*
 git config --global credential.helper store
 
-# install miniconda
-MINICONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" && \
-MINICONDA_TMP_FILE=/tmp/miniconda.sh
-wget "${MINICONDA_URL}" -O ${MINICONDA_TMP_FILE} -q && \
-mkdir -p /opt && \
-sudo bash ${MINICONDA_TMP_FILE} -b -p /opt/conda && \
-sudo chown -R $(echo $USER) /opt/conda && \
-eval "$(/opt/conda/bin/conda shell.bash hook)" && \
-sudo ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
-sudo find /opt/conda/ -follow -type f -name '*.a' -delete && \
-sudo find /opt/conda/ -follow -type f -name '*.js.map' -delete && \
-conda update --all && \
-conda clean -afy && \
-conda init bash && \
-conda init zsh
-
-
-# install homebrew
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
-echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
-
-# install vscode
-sudo apt-get install wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code
-
-# install zsh
+# Install zsh
 cd ${HOME}
 oh_my_install_dir="${HOME}/.oh-my-zsh"
 echo ">>>>> Inside if statement oh-my-zsh configuration... >>>>>"
@@ -89,4 +55,39 @@ echo -e "$(cat "${template_path}")\nDISABLE_AUTO_UPDATE=false\nDISABLE_UPDATE_PR
 git clone https://github.com/zsh-users/zsh-autosuggestions ${oh_my_install_dir}/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${oh_my_install_dir}/custom/plugins/zsh-syntax-highlighting
 echo 'zsh' >> ~/.bashrc
+
+# Install JetBrains Mono font
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+
+# install miniconda
+MINICONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" && \
+MINICONDA_TMP_FILE=/tmp/miniconda.sh
+wget "${MINICONDA_URL}" -O ${MINICONDA_TMP_FILE} -q && \
+mkdir -p /opt && \
+sudo bash ${MINICONDA_TMP_FILE} -b -p /opt/conda && \
+sudo chown -R $(echo $USER) /opt/conda && \
+eval "$(/opt/conda/bin/conda shell.bash hook)" && \
+sudo ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+sudo find /opt/conda/ -follow -type f -name '*.a' -delete && \
+sudo find /opt/conda/ -follow -type f -name '*.js.map' -delete && \
+conda update --all && \
+conda clean -afy && \
+conda init bash && \
 conda init zsh
+
+# install homebrew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
+
+# install vscode
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
