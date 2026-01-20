1b24807fcd5aa94e54f3d3257c94c90efc161fb8
Hadir <halexandergc@gmail.com>
2026-01-20 20:48:54 -0300

    update Dockerfile to include graphviz and graphviz-dev; add gitweb directory creation in build process; add docker-compose.yml for rustdesk services; update package installation script for GIMP and Wacom driver

--- setup_devenv/windows/packages.ps1
+++ setup_devenv/windows/packages.ps1
@@ -32,7 +32,7 @@ winget install --id Microsoft.AzureCLI -e --source winget
 winget install --id Microsoft.Azd -e --source winget
 
 #>>> Optional and interesting stuff
-# winget install --id GIMP.GIMP -e --source winget
+# winget install --id GIMP.GIMP.3 -e --source winget
 # winget install --id HandBrake.HandBrake -e --source winget
 # winget install --id Inkscape.Inkscape -e --source winget
 # winget install --id Insomnia.Insomnia -e --source winget
@@ -43,13 +43,14 @@ winget install --id Microsoft.Azd -e --source winget
 # winget install --id PuTTY.PuTTY -e --source winget
 # winget install --id Nushell.Nushell -e --source winget
 # winget install --id OpenWhisperSystems.Signal -e --source winget
-# winget install --id Wacom.WacomTabletDrive -e --source winget
+# winget install --id Wacom.WacomTabletDriver -e --source winget
 
 # Other urls to download packages
 # ==============================
 # jetbrainsMono: https://github.com/JetBrains/JetBrainsMono/releases/latest
 # kaspersky: https://my.kaspersky.com/
 # mingw-w64: https://github.com/msys2/msys2-installer/releases/download/nightly-x86_64/msys2-x86_64-latest.exe
+# make-w32: https://sourceforge.net/projects/ezwinports/files/make-4.4.1-without-guile-w32-bin.zip/download
 # miniconda: https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
 # vcredistributable: https://aka.ms/vs/17/release/vc_redist.x64.exe
 # virtualbox: https://download.virtualbox.org/virtualbox/LATEST.TXT
