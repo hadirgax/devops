c43eec5fc4740e23f8026074770b3af61be2ed11
Hadir Alexander Garcia <halexandergc@gmail.com>
2026-03-01 18:00:39 -0300

    feat: Update Miniconda DevContainer template files, remove RustDesk Docker Compose, and refine Windows winget package installations.

--- setup_devenv/windows/packages.ps1
+++ setup_devenv/windows/packages.ps1
@@ -1,7 +1,5 @@
 # >>> Default tools
 winget install --id 7zip.7zip -e --source winget
-winget install --id Anaconda.Miniconda3 -e --source winget
-winget install --id Brave.Brave -e --source winget
 winget install --id calibre.calibre -e --source winget
 winget install --id dbeaver.dbeaver -e --source winget
 winget install --id Docker.DockerDesktop -e --source winget
@@ -10,8 +8,8 @@ winget install --id FastStone.Viewer -e --source winget
 winget install --id Git.Git -e --source winget
 winget install --id Google.Chrome -e --source winget
 winget install --id Google.GoogleDrive -e --source winget
-winget install --id JetBrains.Toolbox -e --source winget
 winget install --id Microsoft.DotNet.DesktopRuntime.6 -e --source winget
+winget install --id Microsoft.OneDrive -e --source winget
 winget install --id Microsoft.PowerShell -e --source winget
 winget install --id Microsoft.PowerToys -e --source winget
 winget install --id Microsoft.Teams -e --source winget
@@ -22,28 +20,31 @@ winget install --id Mobatek.MobaXterm -e --source winget
 winget install --id Mozilla.Firefox -e --source winget
 winget install --id Notepad++.Notepad++ -e --source winget
 winget install --id Notion.Notion -e --source winget
+winget install --id Postman.Postman -e --source winget
 winget install --id SMPlayer.SMPlayer -e --source winget
 winget install --id Spotify.Spotify -e --source winget
-winget install --id Stoplight.Studio -e --source winget
 winget install --id Tarma.PublishOrPerish -e --source winget
 winget install --id TrackerSoftware.PDF-XChangeEditor -e --source winget
 winget install --id Zoom.Zoom -e --source winget
 
 #>>> Optional and interesting stuff
+# winget install --id Anaconda.Miniconda3 -e --source winget
+# winget install --id Brave.Brave -e --source winget
 # winget install --id GIMP.GIMP.3 -e --source winget
 # winget install --id HandBrake.HandBrake -e --source winget
 # winget install --id Inkscape.Inkscape -e --source winget
 # winget install --id Insomnia.Insomnia -e --source winget
+# winget install --id JetBrains.Toolbox -e --source winget
 # winget install --id KDE.Kdenlive -e --source winget
 # winget install --id KDE.Krita -e --source winget
 # winget install --id Maximus5.ConEmu -e --source winget
 # winget install --id Microsoft.Azd -e --source winget
 # winget install --id Microsoft.AzureCLI -e --source winget
 # winget install --id Neovim.Neovim -e --source winget
-# winget install --id Postman.Postman -e --source winget
-# winget install --id PuTTY.PuTTY -e --source winget
 # winget install --id Nushell.Nushell -e --source winget
 # winget install --id OpenWhisperSystems.Signal -e --source winget
+# winget install --id PuTTY.PuTTY -e --source winget
+# winget install --id Stoplight.Studio -e --source winget
 # winget install --id Wacom.WacomTabletDriver -e --source winget
 
 # Other urls to download packages
